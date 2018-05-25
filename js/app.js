331f747744c2166b75618214321e0529b288db6b
CL4uD14 <31929720+CL4uD14@users.noreply.github.com>
2018-05-25 13:27:34 +0200

    add color and background functions

---
+++ js/app.js
@@ -0,0 +1,25 @@
+function getTheColor( colorVal ) {
+  	var theColor = "#0DB8B5";
+
+  	if ( colorVal < 20 ) {
+			    theColor = "#D92727";
+		  }
+	  else if	( colorVal < 40 ) {
+			    theColor = "#FC8F12";
+		  }
+    else if	( colorVal < 60 ) {
+          theColor = "#FFE433";
+      }
+    else if	( colorVal < 80 ) {
+          theColor = "#6FCC43";
+      }
+
+  	return( theColor );
+}
+
+function refreshSwatch() {
+	var slider = $( ".slider" ).slider( "value" ),
+	myColor = getTheColor( slider );
+
+	$( "#background" ).css( "background-color", myColor );
+}
